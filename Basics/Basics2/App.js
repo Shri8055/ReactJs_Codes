c015cd49d1e5b8c56d69b4f3b513f868fc4ce888
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-10-16 14:19:56 +0530

    Add files via upload

---
+++ Basics/Basics2/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Header1,Header2,Header3} from './component/header';
+function Mycomponent(props) {
+  return <div>{props.name}</div>;
+}
+function App() {
+  return <div> Hello
+    <Header1 />
+    <Header2 />
+    <Header3 />
+    Guys
+  </div>;
+}
+export default App;
