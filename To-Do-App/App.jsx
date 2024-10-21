3015977b7a8e0a9d3d6f17472c48ae7f6958fd28
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-10-21 12:24:57 +0530

    Add files via upload

---
+++ To-Do-App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Ui/Header';
+import Home from './Ui/Home';
+function App(){
+    return(
+        <div>
+            <Header/>
+            <Home/>
+        </div>
+    );
+}
+export default App;
